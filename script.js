6d987d8739dcf29314f8a0394a15766a7a507a85
Semidime <semidime@protonmail.com>
2023-05-18 11:28:41 +0100

    updates to divideByZero function.
    Changes to transition timings of mainDisplay.
    Display resets to zero at end of function.

--- script.js
+++ script.js
@@ -199,16 +199,25 @@ function removeSelected() {
 }
 
 function divideByZero() {
-
     minorDisplay1.textContent = `${formatDisplay(firstNumber)} ${operator}`
     minorDisplay2.textContent = `${formatDisplay(secondNumber)} =`
     mainDisplay.textContent = "I'm sorry Dave";
     mainDisplay.style.opacity = "0";
 
     setTimeout(() => {
-        mainDisplay.textContent = "I'm afraid I can't do that";
-        mainDisplay.style.opacity = "1";        
-    },1800);
+        mainDisplay.style.opacity = "1";
+        mainDisplay.textContent = "I'm afraid I can't do that"; 
+    },2000);
+
+    setTimeout(() => {
+        mainDisplay.style.opacity = "0";  
+    },4000);
+
+    setTimeout(() => {
+        displayValue = "0";
+        mainDisplay.style.opacity = "1";
+        mainDisplay.textContent = "0";  
+    },6000);
 }
 
 /* Mouse inputs */
